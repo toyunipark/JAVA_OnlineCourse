c966dfca1cbbcd4ae31317a4f067a14b11219531
toyunipark <58938738+toyunipark@users.noreply.github.com>
2019-12-19 12:06:23 +0900

    Chapter5. 3./4. 함수(Function)와 메서드(Method)

---
+++ src/chapter5_3_function/FunctionTest.java
@@ -0,0 +1,41 @@
+package chapter5_3_function;
+
+public class FunctionTest {
+
+	public static int addNum(int num1, int num2) { // return 값이 정수형(integer)이므로, int로 선언
+		int result;
+		result = num1+num2;
+		return result;
+	}
+	
+	public static void sayHello(String greeting) { // return 값이 없을 때 => void
+		System.out.println(greeting);
+	}
+	
+	
+	public static int calcSum() {
+		int sum=0;
+		int i;
+		
+		for (i = 0; i <= 100; i++) {
+			sum+=i;
+		}
+		
+		return sum;
+	}
+	
+	
+	public static void main(String[] args) {
+		int n1 = 10;
+		int n2 = 20;
+	
+		int total = addNum(n1, n2);
+		System.out.println(total);
+		
+		sayHello("HELLO");
+		
+		int calResult=calcSum();
+		System.out.println(calResult);
+	}
+
+}

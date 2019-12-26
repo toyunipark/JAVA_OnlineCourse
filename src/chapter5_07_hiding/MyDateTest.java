ecf6de586ddf36a9e492146639c03b08fe2e770b
toyunipark <58938738+toyunipark@users.noreply.github.com>
2019-12-26 12:41:42 +0900

    Chapter5. 클래스와 객체 - 9. 10. 정보은닉

---
+++ src/chapter5_07_hiding/MyDateTest.java
@@ -0,0 +1,16 @@
+package chapter5_07_hiding;
+
+public class MyDateTest {
+
+	public static void main(String[] args) {
+		
+		MyDate date = new MyDate();
+
+		date.setYear(2019);
+		date.setMonth(0);
+		date.setDay(26);
+		
+		date.showDate();
+	}
+
+}
